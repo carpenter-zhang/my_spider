e01bbe3b264dbebcac6810ccf76e7e53f3a0fa4c
zzzxxl <18505652913@163.com>
2019-04-24 21:16:46 +0800

    douban python_book

--- 2.doubanbook_python/2. douban_book.py
+++ 2.doubanbook_python/2. douban_book.py
@@ -8,9 +8,9 @@ from lxml import etree
 
 class DoubanPythonSpider(object):
     def __init__(self):
-        self.start_url = 'https://m.douban.com/j/search/?q=python&t=book&p=0'
         self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 "
                                       "(KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"}
+        self.book_list = []
 
     def start(self, url):
         response = requests.get(url, headers=self.headers)
@@ -20,25 +20,33 @@ class DoubanPythonSpider(object):
     def html_parse(self, html_str):
         html = etree.HTML(html_str)
         item_list = html.xpath("//li//div[@class='subject-info']")
-        book_list = []
 
         for item in item_list:
             book = {}
             name = item.xpath("./span[@class='subject-title']/text()")[0]
-            book['name'] = name
+            book['name'] = name.split('\n')[0]
 
             rate_num = item.xpath(".//span[@class='rating-stars']/@data-rating")[0]
             rate_num = int(str(rate_num).split('.')[0]) / 10
             book['rate_num'] = rate_num
 
-            book_list.append(book)
+            self.book_list.append(book)
+
 
     def run(self):
-        # 1. 连接
-        html_str = self.start(self.start_url)
+        for i in range(200):
+            try:
+                # 1. 连接
+                start_url = 'https://m.douban.com/j/search/?q=python&t=book&p={}'.format(i)
+                html_str = self.start(start_url)
 
-        # 2. 获取到数据,开始解析
-        self.html_parse(html_str)
+                # 2. 获取到数据,开始解析
+
+                self.html_parse(html_str)
+
+            except Exception as e:
+                print('error')
+                continue
 
 
 if __name__ == '__main__':
